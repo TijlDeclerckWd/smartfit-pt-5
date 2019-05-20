578e4e910cefdab93e0e89ed9a10ddcaf149ae17
Tijl Declerck <tijl.declerck7@gmail.com>
2019-05-20 14:16:08 -0500

    ready for deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import { isDevMode } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,7 +9,9 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'smartfit-PT5';
 
-  constructor(){}
+  constructor() {
+    console.log(isDevMode());
+  }
 
   ngOnInit() {}
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_API_URL: 'https://smartfit-pt-backend.herokuapp.com/api',
+  BASE_BE_URL: 'https://smartfit-pt-backend.herokuapp.com'
 };
